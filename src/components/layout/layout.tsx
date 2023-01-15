4dc71670def8dcef701a655f384c9ee0dbf5796d
mig <michaelleojacob@gmail.com>
2023-01-15 13:43:22 -0800

    added debounce and get window size hook. Using window size in layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,9 +5,15 @@ import AuthContent from "../auth/authContent/authContent";
 import { SnackBarProvider } from "../context/snackbar/snackbar";
 import { useAppContext } from "../context/appContext/appContext";
 import { ProjectProvider } from "../context/projectContext/projectContext";
+import useWindowDimensions from "../customHook/useWindowDimensions";
+import { useEffect } from "react";
 
 const Layout = () => {
   const { isLoggedIn } = useAppContext();
+  const { height, width } = useWindowDimensions();
+  useEffect(() => {
+    console.log(width);
+  }, [width]);
   return (
     <div>
       <SnackBarProvider>
