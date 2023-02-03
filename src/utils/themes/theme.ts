0118fa317ada22dc545b3e10b447b7c1bd8159e5
mig <michaelleojacob@gmail.com>
2023-02-03 14:59:53 -0800

    custom theme color neutral working, it is not implemented but confirmed via a test that it does work.

--- src/utils/themes/theme.ts
+++ src/utils/themes/theme.ts
@@ -1,4 +1,4 @@
-import { ThemeOptions } from "@mui/material";
+import { ThemeOptions, createTheme } from "@mui/material";
 
 /**
  * main types:
@@ -7,10 +7,40 @@ import { ThemeOptions } from "@mui/material";
  * main, dark, light *plus contrastText
  */
 
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: "#64748B",
+      contrastText: "#fff",
+    },
+  },
+});
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    neutral: Palette["primary"];
+  }
+
+  // allow configuration using `createTheme`
+  interface PaletteOptions {
+    neutral?: PaletteOptions["primary"];
+  }
+}
+
+// Update the Button's color prop options
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    neutral: true;
+  }
+}
+
 export const darkTheme: ThemeOptions = {
   palette: {
     mode: "dark",
-    background: {},
+    neutral: {
+      main: "#D81B60",
+      light: "#262626",
+    },
   },
 };
 
