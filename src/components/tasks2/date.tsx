2f405c42d102836e5b8dc15fbf65f5146aba8f0a
mig <michaelleojacob@gmail.com>
2023-03-31 20:25:32 -0700

    using dayjs to render when the task is due. And the relative time/days between now and then.

---
+++ src/components/tasks2/date.tsx
@@ -0,0 +1,63 @@
+import { Box } from "@mui/system";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
+dayjs.extend(relativeTime);
+dayjs.extend(updateLocale);
+
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "due in %s",
+    past: "due %s ago",
+    s: "a few seconds",
+    m: "a minute",
+    mm: "%d minutes",
+    h: "an hour",
+    hh: "%d hours",
+    d: "a day",
+    dd: "%d days",
+    M: "a month",
+    MM: "%d months",
+    y: "a year",
+    yy: "%d years",
+  },
+});
+
+const TaskDate = ({ taskdue }: { taskdue: Date }) => {
+  const today = dayjs(new Date()).format("YYYY-MM-DD");
+  const due = dayjs(taskdue).format("YYYY-MM-DD");
+
+  return (
+    <Box>
+      {dayjs(today).isSame(due)
+        ? "due today"
+        : dayjs(due).isAfter(today)
+        ? dayjs(today).to(due)
+        : dayjs(due).from(today)}
+    </Box>
+  );
+};
+
+export default TaskDate;
+
+/**
+ * examples:
+ *
+ * const today = dayjs(new Date()).format("YYYY-MM-DD");
+ * const date = dayjs(task.due).format("YYYY/MM/DD");
+ * const date2 = dayjs(task.due).format("YYYY-MM-DD");
+ * dayjs(task.due).isAfter(today)
+ * dayjs(today).to(date)
+ * dayjs(date).from(today)
+ */
+
+/**
+ * firstly checking for if the task is due today.
+ * If it is not, it goes on to check if it due in the future or past due.
+ * 
+ *   {dayjs(today).isSame(due)
+        ? "due today"
+        : dayjs(due).isAfter(today)
+        ? `due ${dayjs(today).to(due)}`
+        : dayjs(due).from(today)}
+ */
--- src/components/tasks2/taskCard.tsx
+++ src/components/tasks2/taskCard.tsx
@@ -1,17 +1,14 @@
 import { Todo } from "../../types/types";
 import { Box } from "@mui/system";
-import dayjs from "dayjs";
+import TaskDate from "./date";
 
 const TaskCard2 = ({ task }: { task: Todo }) => {
-  const today = new Date();
-  const date = dayjs(task.due).format("YYYY/MM/DD");
-  const date2 = dayjs(task.due).format("YYYY-MM-DD");
-  console.log(today, date, date2);
   return (
-    <Box>
-      <div>{task.title}</div>
-      {task.due ? <div>{dayjs(task.due).format("DD/MM/YYYY")}</div> : null}
-      {/* {task.due ? <div>{dayjs(new Date()).to(date)}</div> : null} */}
+    <Box sx={{ display: "flex", flexDirection: "row" }}>
+      <div>
+        {task.title} {task.prio}
+      </div>
+      {task.due ? <TaskDate taskdue={task.due} /> : null}
     </Box>
   );
 };
